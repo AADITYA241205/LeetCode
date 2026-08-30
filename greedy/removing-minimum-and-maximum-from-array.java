b54e751683dc6382b176b9ef57a3a1641f2f3862
Aaditya Siddharth Bansod <aadityasbansod@gmail.com>
2026-08-30 21:17:55 +0530

    LC-removing-minimum-and-maximum-from-array [Runtime: N/A; Memory: N/A]

---
+++ greedy/removing-minimum-and-maximum-from-array.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int minimumDeletions(int[] nums) {
+
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        int imin = 0;
+        int imax = 0;
+
+        for(int i = 0 ; i<nums.length ; i++){
+
+            if(min>nums[i]){
+                min = nums[i];
+                imin = i;
+            }
+
+            if(max<nums[i]){
+                max = nums[i];
+                imax = i;
+            }
+
+        }
+
+        int ans = Math.max(imin,imax)+1;
+        
+        if(imin>imax){
+            ans = Math.min(ans,imin+ 1 + (nums.length - imax));
+            ans = Math.min(ans,imax+1 +(nums.length - imin));
+        }
+        else if(imin<imax){
+            ans = Math.min(ans,imax+1+ (nums.length - imin));
+            ans = Math.min(ans,imin+1+(nums.length - imax));
+        }
+        return ans;
+        
+    }
+}
