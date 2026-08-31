3aa34e78eddfb9bd0adc85ae3ffd15efe14fa004
Aaditya Siddharth Bansod <aadityasbansod@gmail.com>
2026-08-31 09:18:21 +0530

    LC-find-the-middle-index-in-array [Runtime: N/A; Memory: N/A]

--- array/find-the-middle-index-in-array.java
+++ array/find-the-middle-index-in-array.java
@@ -1,11 +1,8 @@
 class Solution {
     public int findMiddleIndex(int[] nums) {
 
-        int[] psum = new int[nums.length];
-
         int sum = 0;
         for(int i = 0 ; i<nums.length ; i++){
-            psum[i] = sum;
             sum+=nums[i];
         }
         int ssum = 0;
