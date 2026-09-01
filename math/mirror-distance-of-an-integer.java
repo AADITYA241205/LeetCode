7b76266ef74255fd1a856aa04e27c417f96e0905
Aaditya Siddharth Bansod <aadityasbansod@gmail.com>
2026-09-01 23:51:36 +0530

    LC-mirror-distance-of-an-integer [Runtime: 1 ms; Memory: 42.8 MB]

---
+++ math/mirror-distance-of-an-integer.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int mirrorDistance(int n) {
+        
+        int a = n;
+        int rev = 0;
+        while(a>0){
+            rev = rev*10 + a%10;
+            a=a/10;
+        }
+
+        return Math.abs(n-rev);
+    }
+}
