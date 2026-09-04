defa53536303e70bb6e2bfa3a75af2fd2df450e6
Aaditya Siddharth Bansod <aadityasbansod@gmail.com>
2026-09-04 15:44:29 +0530

    LC-smallest-stable-index-i [Runtime: N/A; Memory: N/A]

--- array/smallest-stable-index-i.java
+++ array/smallest-stable-index-i.java
@@ -6,14 +6,14 @@ class Solution {
         for(int i = 0 ; i<nums.length ; i++){
             max = Math.max(max,nums[i]);
 
-            int min = 101;
+            int min = Integer.MAX_VALUE;
             for(int j = i ; j<nums.length ; j++){
                 min = Math.min(min,nums[j]);
             }
 
-            // System.out.println(max +" "+ min);
+            System.out.println(max +" "+ min);
 
-            if(max-min<=k){
+            if((max-min)<=k){
                 ans = i;
                 break;
             }
