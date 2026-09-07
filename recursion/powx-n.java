bf167f4f378c96cf18f0cb10a6520e22ac4ffaa4
Aaditya Siddharth Bansod <aadityasbansod@gmail.com>
2026-09-08 03:15:59 +0530

    LC-powx-n [Runtime: 0 ms; Memory: 47.8 MB]

--- recursion/powx-n.java
+++ recursion/powx-n.java
@@ -1,25 +1,28 @@
 class Solution {
+    public double power(double x , long n){
+        if(n==0){
+            return 1;
+        }
+        if(n==1){
+            return x;
+        }
+
+        if(n%2==0){
+            return power(x*x,n/2);
+        }
+
+        return x*power(x,n-1);
+
+    }
     
     public double myPow(double x, int n) {
-        int i = 0;
-        long nn = n;
-        double ans = 1;
 
+        long nn = n;
         if(nn<0)nn = (-1)*nn;
 
-        while(nn>0){
-            if(nn%2==0){
-                x = x*x;
-                nn=nn/2;
-            }
-            else{
-                ans = ans *x;
-                nn-=1;
-            }
-        }
-        if(n<0) return (double)(1.0/(double)ans);
 
-        return ans;
+        if(n<0) return (double)(1.0/(double)power(x,nn));
+        return power(x,n);
 
     }
 
