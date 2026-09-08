d6fea8459d31d4c47d355981d0ef68b837a72bbd
Aaditya Siddharth Bansod <aadityasbansod@gmail.com>
2026-09-09 04:02:06 +0530

    LC-maximum-substrings-with-distinct-start [Runtime: N/A; Memory: N/A]

---
+++ string/maximum-substrings-with-distinct-start.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxDistinct(String s) {
+
+        int[] arr = new int[26];
+
+        for(int i = 0 ; i<s.length() ; i++){
+            arr[s.charAt(i)-'a']++;
+        }
+
+        int ans = 0;
+        for(int i = 0 ; i<26 ; i++){
+            if(arr[i]!=0)ans++;
+        }
+
+        return ans;
+        
+    }
+}
